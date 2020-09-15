d4c26c29f239148a205950f203fc19e5d4ef3266
jiaoery <373105324@qq.com>
2020-09-15 14:29:29 +0800

    网易云粒子化效果

--- app/src/main/java/cn/cloudwalk/myapplication/MainActivity.kt
+++ app/src/main/java/cn/cloudwalk/myapplication/MainActivity.kt
@@ -1,12 +1,42 @@
 package cn.cloudwalk.myapplication
 
+import android.animation.ObjectAnimator
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.LinearInterpolator
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.ImageViewTarget
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rotateAnimator:ObjectAnimator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //旋转动画，使用属性动画
+        rotateAnimator = ObjectAnimator.ofFloat(music_avatar, View.ROTATION, 0f, 360f)
+        rotateAnimator.duration = 6000
+        rotateAnimator.repeatCount = -1
+        rotateAnimator.interpolator = LinearInterpolator()
+        loadImage()
     }
+
+
+    private fun loadImage() {
+            Glide.with(this@MainActivity)
+                .load(R.mipmap.icon_music)
+                .circleCrop()
+                .into(object : ImageViewTarget<Drawable>(music_avatar) {
+                    override fun setResource(resource: Drawable?) {
+                        music_avatar.setImageDrawable(resource)
+                    }
+                })
+        rotateAnimator.start()
+    }
+
+
 }
---
+++ app/src/main/java/cn/cloudwalk/myapplication/Testxxx.java
@@ -0,0 +1,21 @@
+package cn.cloudwalk.myapplication;
+
+/**
+ * ClassName: Test<br/>
+ * Description: TODO Description. <br/>
+ * date: 2020/7/10 10:14<br/>
+ *
+ * @author YCKJ1729
+ * @version V1.0.0
+ * @since JDK 1.8
+ */
+public class Testxxx {
+
+    public static void main(String[] args) {
+        String namas = "吉祥学习";
+        char[] chars =namas.toCharArray();
+        for (char cha:chars){
+            System.out.println(cha);
+        }
+    }
+}
